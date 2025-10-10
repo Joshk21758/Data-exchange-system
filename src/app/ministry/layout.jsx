008734e65833abaf22faea1fe1ffc4a3bab30b93
Mwansa Kunda <mwansakunda530@gmail.com>
2025-10-10 09:08:28 +0000

    remove the ministry side bar

--- src/app/ministry/layout.jsx
+++ src/app/ministry/layout.jsx
@@ -1,24 +1,16 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
-import MinistrySidebar from "@/components/layout/ministry-sidebar";
 import MinistryHeader from "@/components/layout/ministry-header";
-import { Sidebar } from "@/components/ui/sidebar";
 
 export default function MinistryDashboardLayout({
   children,
 }) {
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40">
-        <Sidebar>
-          <MinistrySidebar />
-        </Sidebar>
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <MinistryHeader />
-          <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-            {children}
-          </main>
-        </div>
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="flex flex-col sm:gap-4 sm:py-4">
+        <MinistryHeader />
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          {children}
+        </main>
       </div>
-    </SidebarProvider>
+    </div>
   );
 }
--- src/components/layout/ministry-header.jsx
+++ src/components/layout/ministry-header.jsx
@@ -11,12 +11,10 @@ import {
 import { Button } from "@/components/ui/button";
 import { UserNav } from "@/components/layout/user-nav";
 import { ThemeToggle } from "./theme-toggle";
-import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function MinistryHeader() {
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-      <SidebarTrigger className="sm:hidden" />
       <Breadcrumb className="hidden md:flex">
         <BreadcrumbList>
           <BreadcrumbItem>
